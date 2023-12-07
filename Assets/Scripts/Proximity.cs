82110f3e2464cb24b8383310d55db7ac48a2ef8f
kalinswartz <kalinswartz@gmail.com>
2023-12-07 09:58:29 -0600

    video proximity

---
+++ Assets/Scripts/Proximity.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Proximity : MonoBehaviour
+{
+    [SerializeField] private Transform cam;
+
+    float distance = 0f;
+    VideoPlayer videoplayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoplayer = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, cam.position);  
+        if (distance < 1)
+        {
+            distance = 1;
+        }
+        videoplayer.SetDirectAudioVolume(0, 1 / distance);
+    }
+}
